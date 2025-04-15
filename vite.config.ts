a8f0cb44e270a0c97a8c7afdd6e5ff0bc6fa2049
Deepli123554 <79585053+Deepli123554@users.noreply.github.com>
2025-04-15 17:09:50 +0700

    gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/phd-portfolio/', // Change this to match your repository name
+  base: '/phd-portfolio', // Change this to match your repository name
 });
